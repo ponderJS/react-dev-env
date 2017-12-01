c3e808276480968fca28d02f2ab872f6e8791330
ponderJS <okcode@outlook.com>
2017-12-01 12:28:18 +0800

    自动复制依赖文件到打包目录

--- pack.js
+++ pack.js
@@ -2,7 +2,9 @@ process.env.NODE_ENV = 'production';
 
 let webpack = require('webpack'),
     webpackConfig = require('./webpack.config.js'),
-    HtmlWebpackPlugin = require('html-webpack-plugin');
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    CleanWebpackPlugin = require('clean-webpack-plugin'),
+    CopyWebpackPlugin = require('copy-webpack-plugin');
 
 
 // 根据配置生成多页面应用
@@ -16,17 +18,28 @@ Object.keys(webpackConfig.entry).forEach(function (d) {
 });
 
 // 压缩
-if(process.argv[2] == 'compress'){
+if (process.argv[2] == 'compress') {
     webpackConfig.plugins.push(new webpack.optimize.UglifyJsPlugin({
         compress: true
     }));
 }
 
+webpackConfig.plugins.unshift(new CleanWebpackPlugin('./build'));
+webpackConfig.plugins.push(new CopyWebpackPlugin(
+    [
+        { from: './fonts', to: 'fonts/' },
+        { from: './img', to: 'img/' },
+        { from: './lib', to: 'lib/' }
+    ],
+    {
+        copyUnmodified: true
+    }
+));
+
 // 编译
 webpack(webpackConfig).run(function (err, stats) {
     if (err) {
-        throw(err);
+        throw (err);
     }
     console.log('Compiled successfully!');
-    console.log('注意：还需将字体、图片、类库等文件复制到打包目录！');
 });
